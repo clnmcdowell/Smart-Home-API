b19838c71fa5fdef8ead9369978c24ba719a81cd
Colin McDowell <cmcdow@bu.edu>
2025-02-18 12:57:06 -0500

    Add update user function

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from smartHomeAPI import app  # Import your FastAPI app
+from smartHomeAPI import app
 import uuid
 
 client = TestClient(app)  # Test client to make API requests
@@ -22,6 +22,7 @@ def test_create_user():
     assert data["phone_number"] == user_data["phone_number"]
     assert data["email"] == user_data["email"]
 
+# Test getting a user
 def test_get_user():
     user_data = {
         "name": "John Doe",
@@ -38,3 +39,28 @@ def test_get_user():
     data = get_response.json()
     assert data["id"] == user_id
     assert data["name"] == user_data["name"]
+
+# Test updating a user
+def test_update_user():
+    user_data = {
+        "name": "Bob",
+        "phone_number": "5555555555",
+        "email": "bob@gmail.com"
+    }
+    create_response = client.post("/users", json=user_data)
+    assert create_response.status_code == 200
+    user_id = create_response.json()["id"]
+
+    updated_data = {
+        "id": user_id,
+        "name": "Bobby",
+        "phone_number": "5555555555",
+        "email": "bobby@aol.com"
+    }
+    update_response = client.put(f"/users/{user_id}", json=updated_data)
+    assert update_response.status_code == 200
+    updated_user = update_response.json()
+
+    # Check that user was updated correctly 
+    assert updated_user["name"] == "Bobby"
+    assert updated_user["email"] == "bobby@aol.com"
