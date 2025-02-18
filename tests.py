0df9b8bda3dab271b2b3ef3378823bc8299da360
Colin McDowell <cmcdow@bu.edu>
2025-02-18 12:47:58 -0500

    Update variable names to be consistent

--- tests.py
+++ tests.py
@@ -7,29 +7,29 @@ client = TestClient(app)  # Test client to make API requests
 ## USER TESTS ##
 
 # Test creating a user
-def testCreateUser():
-    userData = {
+def test_create_user():
+    user_data = {
         "name": "John Doe",
         "phone_number": "1234567890",
         "email": "john@gmail.com"
     }
 
-    response = client.post("/users", json=userData) # API request to create user
+    response = client.post("/users", json=user_data) # API request to create user
     assert response.status_code == 200 # 200 response code means OK
     data = response.json()
     assert "id" in data  # ID should be generated
-    assert data["name"] == userData["name"]
-    assert data["phone_number"] == userData["phone_number"]
-    assert data["email"] == userData["email"]
+    assert data["name"] == user_data["name"]
+    assert data["phone_number"] == user_data["phone_number"]
+    assert data["email"] == user_data["email"]
 
-def testGetUser():
-    userData = {
+def test_get_user():
+    user_data = {
         "name": "John Doe",
         "phone_number": "1234567890",
         "email": "john@gmail.com"
     }
 
-    create_response = client.post("/users", json=userData)
+    create_response = client.post("/users", json=user_data)
     assert create_response.status_code == 200
     user_id = create_response.json()["id"]
 
@@ -37,4 +37,4 @@ def testGetUser():
     assert get_response.status_code == 200
     data = get_response.json()
     assert data["id"] == user_id
-    assert data["name"] == userData["name"]
+    assert data["name"] == user_data["name"]
